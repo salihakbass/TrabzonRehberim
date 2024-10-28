04369b3ca8b7d35ee993271d864f60f6d410cab8
Salih Akbas <salihhakbass@gmail.com>
2024-10-28 12:46:45 +0300

    Fetch and ParseJsonData methods added

--- app/src/main/java/com/example/trabzonrehberim/MainActivity.kt
+++ app/src/main/java/com/example/trabzonrehberim/MainActivity.kt
@@ -1,20 +1,57 @@
 package com.example.trabzonrehberim
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.storage.FirebaseStorage
+import org.json.JSONArray
+import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var itemAdapter: PlaceAdapter
+    private val itemList = mutableListOf<Place>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        recyclerView = findViewById(R.id.placeRv)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        itemAdapter = PlaceAdapter(itemList)
+        recyclerView.adapter = itemAdapter
+
+        fetchJsonData()
+
+    }
+    private fun fetchJsonData() {
+        val storage = FirebaseStorage.getInstance()
+        val storageRef = storage.reference.child("data/data.json")
+
+        storageRef.getBytes(Long.MAX_VALUE).addOnSuccessListener { bytes ->
+            val jsonStr = String(bytes, Charset.defaultCharset())
+            parseJsonData(jsonStr)
+        }.addOnFailureListener { exception ->
+            println("Dosya indirilemedi: ${exception.message}")
         }
     }
+    private fun parseJsonData(jsonStr: String) {
+        val jsonArray = JSONArray(jsonStr)
+        for (i in 0 until jsonArray.length()) {
+            val itemObject = jsonArray.getJSONObject(i)
+            val name = itemObject.getString("name")
+            val description = itemObject.getString("description")
+            val imageUrl = itemObject.getString("image")
+
+            val item = Place(name, description, imageUrl)
+            itemList.add(item)
+        }
+        itemAdapter.notifyDataSetChanged()
+
+
+    }
+
 }
